c968f8d2a806d1269c64db83220d970f25838e51
Thorbjörn Sundragon <puggan@spiro.se>
2018-09-24 19:48:25 +0200

    init

---
+++ config.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+	return (object) [
+		'software' => 'Exemple',
+		'token' => 'aaaaaaaaaaaaaaaa',
+
+		'composerdir' => dirname(__DIR__),
+		'gitdir' => dirname(__DIR__),
+
+		'dbname' => 'my_database',
+		'dbuser' => 'root',
+		'dbpass' => 'password',
+	];
---
+++ version.magento1.php
@@ -0,0 +1,4 @@
+<?php
+	require dirname(__DIR__) . '/app/Mage.php';
+
+	return Mage::getVersion();
